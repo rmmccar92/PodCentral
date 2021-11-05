739cd9fda3a1efe085052b0a0f4c6b875a8b6d1e
Ryan McCarthy <rmmcc92@aol.com>
2021-11-04 20:59:01 -0400

    renamed the local mongodb

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mernshopping', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false
-});
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/pod_central_db",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  }
+);
 
 module.exports = mongoose.connection;
