4f6be7f625d2efa6030afc77477751f35fb082af
Joey Swafford <joey@swafford.co>
2021-11-11 11:30:45 -0500

    homepage fix

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,24 +1,33 @@
-import React from "react";
-import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
-import Grow from "@mui/material/Grow";
+import React from 'react';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Grow from '@mui/material/Grow';
 
-import podcastLogo from "../assets/podcast-logo.png";
+import podcastLogo from '../assets/podcast-logo.png';
 
 const styles = {
   podcastLogo: {
-    display: "block",
-    marginLeft: "auto",
-    marginRight: "auto",
-    width: "300px",
+    display: 'block',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    width: '150px',
   },
 };
 
 const Home = () => {
   return (
-    <Box sx={{ flexGrow: 1 }}>
+    <Box
+      sx={{
+        flexGrow: 1,
+        backgroundColor: '#17141d',
+        width: '500px',
+        height: '1000px',
+        border: 2,
+        borderColor: 'error.main',
+      }}
+    >
       <Grow
-        style={{ transformOrigin: "0 0 0" }}
+        style={{ transformOrigin: '0 0 0' }}
         {...{ timeout: 2000 }}
         in={true}
       >
@@ -26,14 +35,15 @@ const Home = () => {
           variant="h2"
           component="div"
           align="center"
-          pt={2}
-          sx={{ flexGrow: 1, display: { xs: "block", sm: "block" } }}
+          color="white"
+          pt={20}
+          sx={{ flexGrow: 1, display: { xs: 'block', sm: 'block' } }}
         >
           Interacting with podcast content has never been easier
         </Typography>
       </Grow>
       <Grow
-        style={{ transformOrigin: "0 0 0" }}
+        style={{ transformOrigin: '0 0 0' }}
         {...{ timeout: 2000 }}
         in={true}
       >
@@ -41,15 +51,16 @@ const Home = () => {
           variant="h4"
           component="div"
           align="center"
+          color="white"
           pt={5}
-          sx={{ flexGrow: 1, display: { xs: "block", sm: "block" } }}
+          sx={{ flexGrow: 1, display: { xs: 'block', sm: 'block' } }}
         >
           PodCentral is an easy way to manage your own podcast and discover
           podcasts that you’ll love
         </Typography>
       </Grow>
       <Grow
-        style={{ transformOrigin: "0 0 0" }}
+        style={{ transformOrigin: '0 0 0' }}
         {...{ timeout: 2000 }}
         in={true}
       >
