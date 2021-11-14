666f3d34c9167f6feadef753e6540f841558f398
Ryan McCarthy <rmmcc92@aol.com>
2021-11-13 21:05:35 -0500

    tried to fix the api call for specific categories not working yet

--- client/src/components/CategoryCard/index.js
+++ client/src/components/CategoryCard/index.js
@@ -1,26 +1,37 @@
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
-import CardMedia from '@mui/material/CardMedia';
-import { Link } from 'react-router-dom';
+import * as React from "react";
+import { useState, useEffect } from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import CardMedia from "@mui/material/CardMedia";
+import { Link } from "react-router-dom";
 
 export default function CategoryCard(props) {
+  // Doesn't work still returning the same set of podcasts
+  const [podcastData, setPodcastData] = useState("");
 
+  const handleClick = () => {
+    fetch(`/api/${props.category}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setPodcastData(data);
+        console.log(`${props.category}Podcasts:`, data);
+      });
+  };
   return (
     <div>
       <Card
         className="podCard"
-        style={{ backgroundColor: '#17141d', boxShadow: '-1rem 0 3rem #000' }}
+        style={{ backgroundColor: "#17141d", boxShadow: "-1rem 0 3rem #000" }}
         sx={{
           border: 1,
-          borderColor: 'black',
-          borderRadius: '5px',
-          mt: '-20px',
+          borderColor: "black",
+          borderRadius: "5px",
+          mt: "-20px",
         }}
       >
         <CardMedia
-          sx={{ pl: '20px', pr: '20px', pt: '20px' }}
+          sx={{ pl: "20px", pr: "20px", pt: "20px" }}
           component="img"
           height="150"
           image={props.image}
@@ -29,11 +40,10 @@ export default function CategoryCard(props) {
         <CardContent>
           <Typography
             gutterBottom
-            component={Link}
-            to={props.redirect}
+            onClick={handleClick}
             variant="h5"
             color="white"
-            sx={{ ml: '5px', fontWeight: '', fontSize: '16px' }}
+            sx={{ ml: "5px", fontWeight: "", fontSize: "16px" }}
           >
             {props.category}
           </Typography>
