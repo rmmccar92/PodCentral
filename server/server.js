6adeb5cfa7b7ece0ac0b70d254c3bad55bcdb6d7
Ryan McCarthy <rmmcc92@aol.com>
2021-11-16 22:50:29 -0500

    somehow the heroku bug snuck back in

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ app.use(express.json());
 
 // Serve up static assets
 app.use("/images", express.static(path.join(__dirname, "../client/images")));
-app.use(express.static(path.join(__dirname, "../client/public")));
+// app.use(express.static(path.join(__dirname, "../client/public")));
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
