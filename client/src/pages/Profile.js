bd28aa306d605fae4e3b3b44cbff52e8ae2c6045
Joey Swafford <joey@swafford.co>
2021-11-18 12:30:48 -0500

    update profile page

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,30 +1,86 @@
-import * as React from "react";
-import { Link } from "react-router-dom";
-import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
-import Grid from "@mui/material/Grid";
-import Grow from "@mui/material/Grow";
-import { GET_ME } from "../utils/queries";
-import { useQuery } from "@apollo/client";
+import * as React from 'react';
+import { Link } from 'react-router-dom';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+import Grow from '@mui/material/Grow';
+import { GET_ME } from '../utils/queries';
+import { useQuery } from '@apollo/client';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import CategoryCard from '../components/CategoryCard';
+import { IconButton } from '@mui/material';
+import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
+import ChatIcon from '@mui/icons-material/Chat';
+import SettingsIcon from '@mui/icons-material/Settings';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import PropTypes from 'prop-types';
+import { useTheme } from '@mui/material/styles';
+import SwipeableViews from 'react-swipeable-views';
 
-import podcastLogo from "../assets/podcast-logo-2.png";
-import spaceShipLogo from "../assets/space-ship-logo.png";
+import podcastLogo from '../assets/podcast-logo-2.png';
+import spaceShipLogo from '../assets/space-ship-logo.png';
 
 const styles = {
   logo: {
-    display: "block",
-    marginLeft: "auto",
-    marginRight: "auto",
-    width: "125px",
+    display: 'block',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    width: '125px',
   },
   spaceLogo: {
-    display: "block",
-    marginLeft: "auto",
-    marginRight: "auto",
-    width: "190px",
+    display: 'block',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    width: '190px',
   },
 };
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`full-width-tabpanel-${index}`}
+      aria-labelledby={`full-width-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `full-width-tab-${index}`,
+    'aria-controls': `full-width-tabpanel-${index}`,
+  };
+}
+
 const Profile = () => {
+  const theme = useTheme();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  const handleChangeIndex = (index) => {
+    setValue(index);
+  };
   const { data } = useQuery(GET_ME);
   let user;
 
@@ -33,114 +89,217 @@ const Profile = () => {
   }
 
   return (
-    <Box flexGrow={1}>
-      <Grid container spacing={5} >
-        <Grid item xs={12} md={6}  >
-          <Grow
-            style={{ transformOrigin: "0 0 0" }}
-            {...{ timeout: 2000 }}
-            in={true}
+    <Grid
+      container
+      // spacing={0}
+      display="flex"
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      backgroundColor=""
+    >
+      <Box
+        flexGrow={1}
+        display="flex"
+        // alignItems="center"
+        justifyContent="center"
+        height="auto"
+        width={600}
+        pt={5}
+        pb={5}
+        mt={3}
+        mb={3}
+        sx={{ bgcolor: '#17141d', boxShadow: '-1rem 0 3rem #000', border: 1 }}
+      >
+        <Grid item>
+          <Box
+            display="flex"
+            justifyContent="center"
+            sx={{
+              // border: 1,
+              // borderColor: 'error.main',
+              height: 'auto',
+              width: 'auto',
+            }}
           >
-            <Typography
-              variant="h2"
-              component={Link}
-              to="/publish"
-              align="center"
-              pt={5}
-              sx={{ flexGrow: 1, display: { xs: "block", sm: "block" } }}
-              color="black"
-            >
-              Publish
-            </Typography>
-          </Grow>
-          <Grow
-            style={{ transformOrigin: "0 0 0" }}
-            {...{ timeout: 2000 }}
-            in={true}
-          >
-            <Typography
-              variant="h5"
-              component="div"
-              align="center"
-              pt={5}
-              mx={3}
-              sx={{ flexGrow: 1, display: { xs: "block", sm: "block" } }}
-              color="black"
-            >
-              Upload your own podcast in just a few easy steps. We take care of
-              the hard parts so you can focus on making your best podcast.
-            </Typography>
-          </Grow>
-          <Grow
-            style={{ transformOrigin: "0 0 0" }}
-            {...{ timeout: 2000 }}
-            in={true}
-          >
-            <Box pt={3}>
-              <a href="/publish">
-                <img
-                  src={podcastLogo}
-                  alt="space ship - discover logo"
-                  style={styles.logo}
+            <Grid item>
+              <Box>
+                <Avatar
+                  alt="Travis Howard"
+                  src="/broken-image.jpg"
+                  // display="flex"
+                  sx={{ height: '90px', width: '90px' }}
                 />
-              </a>
-            </Box>
-          </Grow>
+              </Box>
+            </Grid>
+          </Box>
+          <Typography
+            // display="flex"
+            // alignItems="center"
+            variant="h4"
+            color="white"
+          >
+            <Grid item>
+              <Box display="flex" justifyContent="center" mt={2}>
+                <Typography>
+                  <span className="profileName">Joey Swafford</span>
+                  {/* <br />
+                  <span className="profileFollowers">No Followers</span> */}
+                </Typography>
+              </Box>
+              <Grid item>
+                <Box
+                  display="flex"
+                  justifyContent="space-between"
+                  mt={4}
+                  height="auto"
+                  width="300px"
+                  sx={{ borderBottom: 2, borderColor: 'black' }}
+                >
+                  <IconButton
+                    className="grow1"
+                    component={Link}
+                    to="/"
+                    size="large"
+                    align="center"
+                    aria-label="menu"
+                    sx={{ color: '#f5b727' }}
+                  >
+                    <NotificationsIcon sx={{ fontSize: '50px' }} />
+                  </IconButton>
+                  <IconButton
+                    className="grow1"
+                    component={Link}
+                    to="/"
+                    size="large"
+                    align="center"
+                    aria-label="menu"
+                    sx={{ color: '#f5b727' }}
+                  >
+                    <ChatIcon sx={{ fontSize: '50px' }} />
+                  </IconButton>
+                  <IconButton
+                    className="grow1"
+                    component={Link}
+                    to="/"
+                    size="large"
+                    align="center"
+                    aria-label="menu"
+                    sx={{ color: '#f5b727' }}
+                  >
+                    <SettingsIcon sx={{ fontSize: '50px' }} />
+                  </IconButton>
+                </Box>
+                <Tabs
+                  value={value}
+                  onChange={handleChange}
+                  indicatorColor="secondary"
+                  textColor="inherit"
+                  variant="fullWidth"
+                  aria-label="full width tabs example"
+                >
+                  <Tab label="Home" {...a11yProps(0)} />
+                  <Tab label="Uploads" {...a11yProps(1)} />
+                  <Tab label="About" {...a11yProps(2)} />
+                </Tabs>
+                <SwipeableViews
+                  axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+                  index={value}
+                  onChangeIndex={handleChangeIndex}
+                >
+                  <TabPanel value={value} index={0} dir={theme.direction}>
+                    Item One
+                  </TabPanel>
+                  <TabPanel value={value} index={1} dir={theme.direction}>
+                    <Grid
+                      item
+                      spacing={3}
+                      display="flex"
+                      direction="row"
+                      justifyContent="space-evenly"
+                      alignItems="center"
+                    >
+                      <Box
+                        // container
+                        // spacing={3}
+                        display="flex"
+                        direction="row"
+                        justifyContent="space-evenly"
+                        // alignItems="center"
+                        // sx={{ pb: '75px', pl: '50px', pr: '50px' }}
+                      >
+                        <Grid item xs={12} md={8} lg={2}>
+                          <CategoryCard />
+                        </Grid>
+                        <Grid item xs={12} md={8} lg={2}>
+                          <CategoryCard />
+                        </Grid>
+                        <Grid item xs={12} md={8} lg={2}>
+                          <CategoryCard />
+                        </Grid>
+                        <Grid item xs={12} md={8} lg={2}>
+                          <CategoryCard />
+                        </Grid>
+                        <Grid item xs={12} md={8} lg={2}>
+                          <CategoryCard />
+                        </Grid>
+                      </Box>
+                    </Grid>
+                  </TabPanel>
+                  <TabPanel value={value} index={2} dir={theme.direction}>
+                    Item Three
+                  </TabPanel>
+                </SwipeableViews>
+              </Grid>
+              <Grid item>
+                <Box
+                  display="flex"
+                  mt={2}
+                  pl={2}
+                  pr={2}
+                  direction="row"
+                  justifyContent="space-between"
+                  // height="auto"
+                  // width="auto"
+                  // sx={{ border: 1 }}
+                >
+                  <Typography>
+                    <span className="followerCount">2,360</span>
+                    <br />
+                    <span className="follower">followers</span>
+                  </Typography>
+                  <Typography>
+                    <span className="followerCount">20,000</span>
+                    <br />
+                    <span className="follower">listens</span>
+                  </Typography>
+                </Box>
+              </Grid>
+              <Grid item>
+                <Box
+                  // display="flex"
+                  // justifyContent="center"
+                  ml="-50px"
+                  mr="-50px"
+                  mt={3}
+                  height="225px"
+                  // width="300px"
+                  style={{
+                    backgroundColor: '#17141d',
+                    boxShadow: '-1rem 0 3rem #000',
+                    border: 1,
+                    borderRadius: '5px',
+                    borderColor: 'black',
+                  }}
+                >
+                  <Typography>Biography or info card</Typography>
+                </Box>
+              </Grid>
+            </Grid>
+          </Typography>
         </Grid>
-        <Grid item xs={12} md={6}>
-          <Grow
-            style={{ transformOrigin: "0 0 0" }}
-            {...{ timeout: 2000 }}
-            in={true}
-          >
-            <Typography
-              variant="h2"
-              component={Link}
-              to="/discover"
-              align="center"
-              pt={5}
-              sx={{ flexGrow: 1, display: { xs: "block", sm: "block" } }}
-              color="black"
-            >
-              Discover
-            </Typography>
-          </Grow>
-          <Grow
-            style={{ transformOrigin: "0 0 0" }}
-            {...{ timeout: 2000 }}
-            in={true}
-          >
-            <Typography
-              variant="h5"
-              component="div"
-              align="center"
-              pt={5}
-              mx={3}
-              sx={{ flexGrow: 1, display: { xs: "block", sm: "block" } }}
-              color="black"
-            >
-              Browse through our extensive library of podcasts to find something
-              you love. We have a podcast for everyone.
-            </Typography>
-          </Grow>
-          <Grow
-            style={{ transformOrigin: "0 0 0" }}
-            {...{ timeout: 2000 }}
-            in={true}
-          >
-            <Box pt={3}>
-              <a href="/discover">
-                <img
-                  src={spaceShipLogo}
-                  alt="space ship - discover logo"
-                  style={styles.spaceLogo}
-                />
-              </a>
-            </Box>
-          </Grow>
-        </Grid>
-      </Grid>
-    </Box >
+      </Box>
+    </Grid>
   );
 };
 
